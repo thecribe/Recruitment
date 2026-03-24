16ac165a1bfad734e42eaa6ce23ed758a91205f2
thecribe <lawaloluwatobi128@gmail.com>
2026-03-24 15:06:04 +0100

    vercel

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,7 +1,6 @@
 export const dynamic = "force-dynamic";
 
 import type { Metadata } from "next";
-import Image from "next/image";
 import PublicRedirectGuard from "../Context/PublicRedirectGuard";
 
 export const metadata: Metadata = {
